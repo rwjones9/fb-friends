26ea1b89f083302d979153b8c5b3dc17fcf1d595
Richard Jones <rwjones9@gmail.com>
2018-05-02 18:49:21 -0400

    Add files via upload

---
+++ friends.py
@@ -0,0 +1,11 @@
+import json
+
+with open('friends_added.json', 'r') as f:
+	friends_dict = json.load(f)
+
+fh = open('friends.txt', 'w')
+for item in friends_dict['friends']:
+	print item['name']
+	fh.write(item['name'])
+	fh.write('\n')
+fh.close()
